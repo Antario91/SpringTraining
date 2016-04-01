6a480e377effa7ece4a16c8c54b4b0cc69f7cf16
Antario91 <sansetboss@gmail.com>
2016-04-01 17:04:01 +0300

    Добавлены классы: PoeticJuggler, Poem. Начата работа над классом Instrumentalist и его полями.

--- src/main/java/Classes/Juggler.java
+++ src/main/java/Classes/Juggler.java
@@ -6,17 +6,21 @@ import Interfaces.Performer;
  * Created by Alexandr on 01.04.2016.
  */
 public class Juggler implements Performer {
-        private int beanBags = 3;
+    private int beanBags = 3;
 
-        public Juggler() {
-        }
+    public Juggler() {
+    }
 
-        public Juggler(int beanBags) {
+    public Juggler(int beanBags) {
             this.beanBags = beanBags;
         }
 
-        public void perform() {
-            System.out.println("JUGGLING " + beanBags + " BEANBAGS");
-        }
+    public void perform() {
+        System.out.println("JUGGLING " + beanBags + " BEANBAGS");
+    }
+
+    public void setBeanBags(int beanBags){
+        this.beanBags = beanBags;
+    }
 }
 
--- src/main/java/SpringTraining/SpringMain.java
+++ src/main/java/SpringTraining/SpringMain.java
@@ -1,5 +1,6 @@
 package SpringTraining;
 
+import Classes.PoeticJuggler;
 import Interfaces.Performer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,11 +12,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringMain
 {
     public static void main( String[] args ){
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("Performers.xml");
-        Performer performer = (Performer) ctx.getBean("dufron");
-        performer.perform();
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("Performers.xml");
+//        Performer performer = (Performer) ctx.getBean("dufron");
+//        performer.perform();
+//
+//        performer = (Performer) ctx.getBean("duke");
+//        performer.perform();
 
-        performer = (Performer) ctx.getBean("duke");
-        performer.perform();
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("Performers.xml");
+        Performer duke = (Performer) ctx.getBean("PoeticDuke");
+        duke.perform();
+        ((PoeticJuggler) duke).setBeanBags(10);
+        Performer dukeE = (Performer) ctx.getBean("PoeticDuke");
+        dukeE.perform();
     }
 }
